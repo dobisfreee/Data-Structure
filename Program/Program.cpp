0a6631501104ef4a5278607b78e37b6f45848e00
dobisfreee <amber981001@gmail.com>
2024-06-13 19:48:47 +0900

    (2024.6.13) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -8,6 +8,12 @@ class DoublyLinkedList
 private:
 	int size;
 
+	struct Node; // prototype 선언만 하고 구현은 안하는
+
+	Node* head;
+	Node* tail;
+
+public:
 	struct Node
 	{
 		T data;
@@ -15,10 +21,6 @@ private:
 		Node* previous;
 	};
 
-	Node* head;
-	Node* tail;
-
-public:
 	DoublyLinkedList()
 	{
 		size = 0;
@@ -119,6 +121,87 @@ public:
 		size++;
 	}
 
+	void PopFront()
+	{
+		if (size == 0)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else 
+		{	
+			Node* deleteNode = head;
+
+			if (size == 1)
+			{
+				head = nullptr;
+				tail = nullptr;
+			}
+			else
+			{
+				deleteNode->next->previous = nullptr;
+				head = head->next;
+			}
+			delete deleteNode;
+			size--;
+		}
+	}
+
+	int& Size()
+	{
+		return size;
+	}
+
+	Node* Begin()
+	{
+		return head; 
+	}
+
+	void Insert(Node* position, T data)
+	{
+		if (head == nullptr)
+		{
+			PushBack(data);
+		}
+		else
+		{
+			Node* previousNode = position;
+			Node* nextNode = position->next;
+
+			if (previousNode->previous == nullptr)
+			{
+				PushFront(data);
+			}
+			else if (nextNode == nullptr)
+			{
+				PushBack(data);
+			}
+			else
+			{
+				Node* newNode = new Node;
+
+				newNode->data = data;
+
+				newNode->previous = previousNode;
+				newNode->next = nextNode;
+				
+				previousNode->next = newNode;
+				nextNode->previous = newNode;
+
+				size++;
+			}
+			
+		}
+		
+	}
+
+	~DoublyLinkedList()
+	{
+		while (head != nullptr)
+		{
+			PopFront();
+		}
+	}
+
 };
 
 int main()
@@ -128,7 +211,13 @@ int main()
 	doublylinkedlist.PushFront(10);
 	doublylinkedlist.PushFront(20);
 	doublylinkedlist.PushFront(30);
+
+	doublylinkedlist.Insert(doublylinkedlist.Begin()->next->next, 99);  // 30 20 10 99 
+	
 	doublylinkedlist.Show();
+
+	
+	// cout << "Double Linked List의 주소값 : " << doublylinkedlist.Begin()->next << endl; 
 	
 
 	return 0;

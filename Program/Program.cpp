f58c2684ed6569a813bfe5dbecb9ff65ebfbb969
dobisfreee <amber981001@gmail.com>
2024-07-02 19:46:20 +0900

    (2024.7.2) 레드 블랙 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -13,14 +13,184 @@ private:
 	};
 
 	Node* root;
+
 public:
+
 	BinarySearchTree()
 	{
 		root = nullptr;
 	}
+
+	Node* Root()
+	{
+		return root;
+	}
+
+	Node* CreateNode(T data)
+	{
+		Node* newNode = new Node();
+
+		newNode->data = data;
+		newNode->left = nullptr;
+		newNode->right = nullptr;
+
+		return newNode;
+	}
+
+	void Insert(T data) // 작으면 왼쪽, 크면 오른쪽
+	{
+		if (root == nullptr)
+		{
+			root = CreateNode(data);
+		}
+		else
+		{
+			Node* currentNode = root;
+	
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data < data)
+				{
+				if (currentNode->right == nullptr)
+				{
+					currentNode->right = CreateNode(data);
+						break; 
+				}
+				else
+				{
+					currentNode = currentNode->right;
+				}
+	 			}
+ 			else if (currentNode->data > data)
+ 			{
+ 				if (currentNode->left == nullptr)
+ 				{
+ 					currentNode->left = CreateNode(data);
+ 					break;
+ 				}
+					else
+					{
+	 					currentNode = currentNode->left;
+	 				}
+	 			}
+	 			else
+	 			{
+	 				return;
+	 			}
+			}
+	 	}
+	 }
+
+	// void Insert(T data, Node * root)
+	// {
+	// 	if (root == nullptr)
+	// 	{
+	// 		root = CreateNode(data);
+	// 	}
+	// 	else
+	// 	{
+	// 		Node* currentNode = root;
+	// 
+	// 		if (currentNode != nullptr)
+	// 		{
+	// 			if (currentNode->data > data)
+	// 			{
+	// 				if (currentNode->left == nullptr)
+	// 				{
+	// 					currentNode->left = CreateNode(data);
+	// 				}
+	// 				else
+	// 				{
+	// 					Insert(data, root->left);
+	// 				}
+	// 			}
+	// 			else if(currentNode->data < data)
+	// 			{
+	// 				if (currentNode->right == nullptr)
+	// 				{
+	// 					currentNode->right = CreateNode(data);
+	// 				}
+	// 				else
+	// 				{
+	// 					Insert(data, root->left);
+	// 				}
+	// 			}
+	// 			else
+	// 			{
+	// 				return;
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	bool Find(T data)
+	{
+		Node* currentNode = root; 
+		if (root == nullptr)
+		{
+		
+			return false;
+		}
+		else
+		{
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data < data)
+				{
+					currentNode = currentNode->right;
+				}
+				else if (currentNode->data > data)
+				{
+					currentNode = currentNode->left;
+				}
+				else 
+				{
+					
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
+	void Inorder(Node * root)
+	{
+		if (root != nullptr)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
+		}
+	}
+
+	void Destroy(Node * root)
+	{
+		if (root != nullptr)
+		{
+			Destroy(root->left);
+			Destroy(root->right);
+			delete root;
+		}
+	}
+
+	~BinarySearchTree()
+	{
+		Destroy(root);
+	}
 };
 int main()
 {
+
+	BinarySearchTree <int> binarySearchTree;
+	
+	binarySearchTree.Insert(10);
+	binarySearchTree.Insert(7);
+	binarySearchTree.Insert(15);
+	binarySearchTree.Insert(5);
+
+	cout << binarySearchTree.Find(2) << endl;
+
+	binarySearchTree.Inorder(binarySearchTree.Root());
 
 	return 0;
 }
